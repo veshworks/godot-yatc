0c55cab80d8a5ce42022373ec72047f352f053cd
Vinícius Hoyer <contact@vhoyer.dev>
2025-06-14 00:26:34 -0300

    ✨ Add base for account new

--- godot-yatc.gd
+++ godot-yatc.gd
@@ -16,12 +16,31 @@ const custom_types = [
 ]
 
 
+@export var client_id: String
+@export var username: String
+@export var scope: Array[String] = [
+	'user:read:chat',
+	'user:write:chat',
+	'channel:read:redemptions',
+	'channel:read:ads',
+	'moderator:manage:announcements',
+	]
+@export var persist: bool = true
+
+
 func _enable_plugin() -> void:
 	for autoload in autoload_singletons:
 		add_autoload_singleton.call(autoload[0], autoload[1])
 
 	for custom_type in custom_types:
 		add_custom_type.callv(custom_type)
+
+	ProjectSettings.add_property_info({
+		"name": "yatc/client_id",
+		"type": TYPE_STRING,
+		"hint": PROPERTY_HINT_PLACEHOLDER_TEXT,
+		"hint_string": "t84x861v4hnvv3mxq7nui0u30r8w1p"
+	})
 
 
 func _disable_plugin():
